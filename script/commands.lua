a8ee50d17638f4447a2ae405da2b6bde9117fba1
Nicolas Lang <git@nicolas-lang.de>
2021-09-11 18:29:28 +0200

    lua fixes

--- script/commands.lua
+++ script/commands.lua
@@ -2,21 +2,21 @@
 -- Register Commands
 -------------------------------------------------------------------------------------
 commands.add_command("rcon", nil, function(command)
-	if command.parameter == "print_tick" then
+	if (command.parameter == "print_tick") then
 		msg = command.tick
 	end
 	
-	if command.parameter == "print_gamespeed" then
+	if (command.parameter == "print_gamespeed") then
 		msg = game.speed
 	end
 	
-	if command.parameter == "print_tickpaused" then
+	if (command.parameter == "print_tickpaused") then
 		msg = game.tick_paused
 	end
 	
-	if command.player_index ~= nil then
+	if (command.player_index ~= nil) then
 		game.get_player(command.player_index).print(command.parameter .. ": " .. tostring(msg))
-	else:
+	else
 		rcon.print(msg)
 	end
 end)
