be3572fb98e166c7944e126eb574560ff84e8d31
Nicolas Lang <git@nicolas-lang.de>
2021-09-11 20:47:11 +0200

    cleanup and additional nil checks

--- script/commands.lua
+++ script/commands.lua
@@ -2,19 +2,24 @@
 -- Register Commands
 -------------------------------------------------------------------------------------
 commands.add_command("rcon", nil, function(command)
-	local msg
-	if (command.parameter == "print_tick") then
-		msg = command.tick
-	end
-	if (command.parameter == "print_gamespeed") then
-		msg = game.speed
-	end
-	if (command.parameter == "print_tickpaused") then
-		msg = game.tick_paused
-	end
-	if (command.player_index ~= nil) then
-		game.get_player(command.player_index).print(command.parameter .. ": " .. tostring(msg))
-	else
-		rcon.print(msg)
+	local reply
+	local parameter = command.parameter
+	if (parameter) then 
+		--check action
+		if (parameter == "print_tick") then
+			reply = command.tick
+		end
+		if (parameter == "print_gamespeed") then
+			reply = game.speed
+		end
+		if (parameter == "print_tickpaused") then
+			reply = game.tick_paused
+		end
+		--reply to source
+		if (command.player_index ~= nil) then
+			game.get_player(command.player_index).print(parameter.. ": " .. tostring(reply))
+		else
+			rcon.print(reply)
+		end
 	end
 end)
