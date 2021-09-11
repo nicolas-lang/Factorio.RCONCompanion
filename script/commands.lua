d16e5beab992b27ad2cb6c525082013a5c9c4bea
Nicolas Lang <git@nicolas-lang.de>
2021-09-11 18:31:57 +0200

    lua style fixes

--- script/commands.lua
+++ script/commands.lua
@@ -2,18 +2,16 @@
 -- Register Commands
 -------------------------------------------------------------------------------------
 commands.add_command("rcon", nil, function(command)
+	local msg
 	if (command.parameter == "print_tick") then
 		msg = command.tick
 	end
-	
 	if (command.parameter == "print_gamespeed") then
 		msg = game.speed
 	end
-	
 	if (command.parameter == "print_tickpaused") then
 		msg = game.tick_paused
 	end
-	
 	if (command.player_index ~= nil) then
 		game.get_player(command.player_index).print(command.parameter .. ": " .. tostring(msg))
 	else
